4fcd9a4783303581d8809c689fc2269446ba8610
Martyfloorball <31356774+Martyfloorball@users.noreply.github.com>
2017-11-01 14:09:40 +0100

    Add files via upload

--- HotelPlazaExercise/Booking.java
+++ HotelPlazaExercise/Booking.java
@@ -1,12 +1,15 @@
 public class Booking {
+    //fields
     public String startDate;
     public String endDate;
     public int numberOfDays;
     public int roomID;
     public int guestID;
-
-    public Booking(){} //default constructor
-
-    /*public Booking(){ //parameters?
-    }*/
+    
+    /*//room prices
+    public int oneBed = 200;
+    public int doubleBed = 250;
+    public int suitBed = 300;
+    */
+    
 }
--- HotelPlazaExercise/Staff.java
+++ HotelPlazaExercise/Staff.java
@@ -1,8 +1,61 @@
-public class Staff {
-    private String title;
-    private String firstName;
-    private String lastName;
-    private int telephoneNumber;
-    private double salary;
+import java.util.*;
+import java.io.*;
 
+class Staff{
+   
+   private int StaffID;
+   private String title;
+   private String firstName;
+   private String lastName;
+   private int phoneNumber;
+   private double Salary;
+   
+   public Staff(){
+   }
+   
+   public Staff(int StaffID, String title, String firstName, String lastName, int phoneNumber, double Salary){
+      this.StaffID = StaffID;
+      this.title = title;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.phoneNumber = phoneNumber;
+      this.Salary = Salary;
+   }
+   public void saveFile(String fileName)throws Exception{
+      PrintStream outputToFile = new PrintStream(new FileOutputStream(fileName, true));
+      outputToFile.print(toString() + "\r\n\n");
+   }
+   public String toString(){
+      return StaffID + " " + title + " " + firstName + " " + lastName + " " + phoneNumber + " " + Salary;
+   }
+   
+   void createNewStaff(Scanner console)throws Exception{
+      System.out.println("Pick a new staff ID number:");
+      StaffID = console.nextInt();
+      System.out.println("Pick a title:");
+      title = console.next();
+      System.out.println("Enter firstname of staffmember:");
+      firstName = console.next();
+      System.out.println("Enter lastname of staffmember:");
+      lastName = console.next();
+      System.out.println("Enter phonenumber of staffmember:");
+      phoneNumber = console.nextInt();
+      System.out.println("Enter salary of staffmember:");
+      Salary = console.nextDouble();
+      saveFile("StaffList.txt");
+
+   }
+   
+   /*void printStaffList()throws Exception{
+      Scanner staffList = new Scanner (new File("StaffList.txt"));
+      while (staffList.hasNextLine()){
+         StaffID = staffList.nextInt();
+         title = staffList.next();
+         firstName = staffList.next();
+         lastName = staffList.next();
+         phoneNumber = staffList.nextInt();
+         Salary = staffList.nextDouble();
+         System.out.println(toString());
+      }
+   }*/    
 }
